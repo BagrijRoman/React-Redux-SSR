2b177ed035f284919926e40748aa574c81a9e4a8
BagrijRoman <bagrijroman@gmail.com>
2018-04-09 19:35:09 +0300

    add client part

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home component </div>
+  return (
+    <div>
+      <div>Home component </div>
+      <button onClick={() => { console.log('the button is clicked'); }}>The button </button>
+    </div>
+  );
 };
 
 export default Home;
