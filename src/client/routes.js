28598cf9b1249fc0341e28ff7c082fb3f2f90dbc
BagrijRoman <bagrijroman@gmail.com>
2018-04-14 23:20:40 +0300

    update routing system

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import App from './app';
 import HomePage from './pages/homePage';
 import UsersListPage from './pages/usersListPage';
 
 export default [
   {
-    ...HomePage,
-    path: '/',
-    exact: true,
-  },
-  {
-    ...UsersListPage,
-    path: '/users',
-  },
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: '/',
+        exact: true,
+      },
+      {
+        ...UsersListPage,
+        path: '/users',
+      },
+    ],
+  }
 ];
