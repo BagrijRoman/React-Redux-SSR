2131f2d192ae00bc125dde831a9b870e1d9c14c5
BagrijRoman <bagrijroman@gmail.com>
2018-04-14 17:54:52 +0300

    update way of data serializing

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config';
+import serealize from 'serialize-javascript';
 
 import routes from '../client/routes';
 
@@ -21,7 +22,7 @@ export default (req, store) => {
       <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+          window.INITIAL_STATE = ${serealize(store.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>    
